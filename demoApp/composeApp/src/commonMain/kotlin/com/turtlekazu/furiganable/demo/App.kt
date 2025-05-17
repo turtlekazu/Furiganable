7a49e38b49a23650dfdf1515476cf3a5e9dfb7aa
Kazuhiro ISHIBASHI <turtlekazu@icloud.com>
2025-05-17 14:13:22 +0900

    fix imports for App

--- demoApp/composeApp/src/commonMain/kotlin/com/turtlekazu/furiganable/demo/App.kt
+++ demoApp/composeApp/src/commonMain/kotlin/com/turtlekazu/furiganable/demo/App.kt
@@ -7,11 +7,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import furiganable.demoapp.composeapp.generated.resources.Res
-import furiganable.demoapp.composeapp.generated.resources.compose_multiplatform
+import furiganable_root.demoapp.composeapp.generated.resources.Res
+import furiganable_root.demoapp.composeapp.generated.resources.compose_multiplatform
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -26,7 +30,10 @@ fun App() {
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Column(
+                    Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     Image(painterResource(Res.drawable.compose_multiplatform), null)
                     Text("Compose: $greeting")
                 }
