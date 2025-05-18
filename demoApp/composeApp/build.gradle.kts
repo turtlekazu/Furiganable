35670f4aede4d4433f313c10995332e5195b0630
Kazuhiro ISHIBASHI <turtlekazu@icloud.com>
2025-05-18 15:18:50 +0900

    add dependency on furiganable and change demo app screen

--- demoApp/composeApp/build.gradle.kts
+++ demoApp/composeApp/build.gradle.kts
@@ -41,6 +41,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(project(":furiganable"))
         }
     }
 }
--- demoApp/composeApp/src/commonMain/kotlin/com/turtlekazu/furiganable/demo/App.kt
+++ demoApp/composeApp/src/commonMain/kotlin/com/turtlekazu/furiganable/demo/App.kt
@@ -1,43 +1,53 @@
 package com.turtlekazu.furiganable.demo
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import furiganable_root.demoapp.composeapp.generated.resources.Res
-import furiganable_root.demoapp.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.painterResource
+import com.turtlekazu.lib.furiganable.TextWithReadingM2
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+        Column(
+            Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            TextWithReadingM2(
+                "これは[試験用[しけんよう]]の[文字列[もじれつ]]です。",
+                style = MaterialTheme.typography.h3,
+            )
+
+            TextWithReadingM2(
+                "これは[試験用[しけんよう]]の[文字列[もじれつ]]です。",
+                style = MaterialTheme.typography.h4,
+            )
+
+            TextWithReadingM2(
+                "これは[試験用[しけんよう]]の[文字列[もじれつ]]です。",
+                style = MaterialTheme.typography.h5,
+            )
+
+            TextWithReadingM2(
+                "これは[試験用[しけんよう]]の[文字列[もじれつ]]です。",
+                style = MaterialTheme.typography.h6,
+            )
+
+            TextWithReadingM2(
+                "これは[試験用[しけんよう]]の[文字列[もじれつ]]です。",
+                style = MaterialTheme.typography.body1,
+            )
+
+            TextWithReadingM2(
+                "これは[試験用[しけんよう]]の[文字列[もじれつ]]です。",
+                style = MaterialTheme.typography.subtitle1,
+            )
         }
     }
 }
