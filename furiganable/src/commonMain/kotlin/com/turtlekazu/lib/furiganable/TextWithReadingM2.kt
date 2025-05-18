9539c9cf1a7496f9544652e2cc14936f87b5cca4
Kazuhiro ISHIBASHI <turtlekazu@icloud.com>
2025-05-18 15:18:19 +0900

    add TextWithReading component for material2

---
+++ furiganable/src/commonMain/kotlin/com/turtlekazu/lib/furiganable/TextWithReadingM2.kt
@@ -0,0 +1,215 @@
+package com.turtlekazu.lib.furiganable
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.foundation.text.appendInlineContent
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.PlaceholderVerticalAlign
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.em
+
+@Composable
+fun TextWithReadingM2(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+    onTextLayout: ((TextLayoutResult) -> Unit)? = null,
+    style: TextStyle = LocalTextStyle.current,
+    showReadings: Boolean = true,
+) {
+    if (text.hasReadings() && showReadings) {
+        val textDataList: List<TextData> = remember(text) {
+            text.toTextData()
+        }
+        val (textContent, inlineContent) =
+            remember(text) {
+                calculateAnnotatedStringM2(
+                    textDataList = textDataList,
+                    showReadings = showReadings,
+                    color = color,
+                    fontSize = fontSize,
+                    fontWeight = fontWeight,
+                    fontFamily = fontFamily,
+                    fontStyle = fontStyle,
+                    letterSpacing = letterSpacing,
+                    textDecoration = textDecoration,
+                    textAlign = textAlign,
+                    lineHeight = lineHeight,
+                    style = style,
+                )
+            }
+
+        Text(
+            text = textContent,
+            inlineContent = inlineContent,
+            modifier = modifier,
+            color = color,
+            fontSize = fontSize,
+            fontWeight = fontWeight,
+            fontFamily = fontFamily,
+            letterSpacing = letterSpacing,
+            textDecoration = textDecoration,
+            textAlign = textAlign,
+            lineHeight = lineHeight,
+            overflow = overflow,
+            softWrap = softWrap,
+            maxLines = maxLines,
+            minLines = minLines,
+            onTextLayout = onTextLayout ?: {},
+            style = style,
+        )
+    } else {
+        Text(
+            text = text,
+            modifier = modifier,
+            color = color,
+            fontSize = fontSize,
+            fontStyle = fontStyle,
+            fontWeight = fontWeight,
+            fontFamily = fontFamily,
+            letterSpacing = letterSpacing,
+            textDecoration = textDecoration,
+            textAlign = textAlign,
+            lineHeight = lineHeight,
+            overflow = overflow,
+            softWrap = softWrap,
+            maxLines = maxLines,
+            minLines = minLines,
+            onTextLayout = onTextLayout,
+            style = style,
+        )
+    }
+}
+
+private fun calculateAnnotatedStringM2(
+    textDataList: List<TextData>,
+    showReadings: Boolean,
+    color: Color,
+    fontSize: TextUnit,
+    fontWeight: FontWeight?,
+    fontFamily: FontFamily?,
+    fontStyle: FontStyle?,
+    lineHeight: TextUnit,
+    textAlign: TextAlign?,
+    textDecoration: TextDecoration?,
+    letterSpacing: TextUnit,
+    style: TextStyle,
+): Pair<AnnotatedString, Map<String, InlineTextContent>> {
+    val inlineContent = mutableMapOf<String, InlineTextContent>()
+
+    return buildAnnotatedString {
+        for (elem in textDataList) {
+            val text = elem.text
+            val reading = elem.reading
+
+            if (reading == null) {
+                append(text)
+                continue
+            }
+
+            val width = (text.length.toDouble() + (text.length - 1) * 0.05).em
+            val mergedStyle = style.merge(
+                color = color,
+                fontSize = fontSize,
+                fontWeight = fontWeight,
+                textAlign = textAlign ?: TextAlign.Unspecified,
+                lineHeight = lineHeight,
+                fontFamily = fontFamily,
+                textDecoration = textDecoration,
+                fontStyle = fontStyle,
+                letterSpacing = letterSpacing
+            )
+            val height = mergedStyle.lineHeight
+            appendInlineContent(text, text)
+            inlineContent[text] = InlineTextContent(
+                // TODO: find out why height and width need magic numbers.
+                placeholder =
+                    Placeholder(
+                        width = width,
+                        height = height,
+                        placeholderVerticalAlign = PlaceholderVerticalAlign.TextCenter,
+                    ),
+                children = {
+                    val readingFontSize = mergedStyle.fontSize / 2
+
+                    Box(
+                        modifier = Modifier
+                            .wrapContentSize(),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Text(
+                            text = text,
+                            color = color,
+                            fontSize = fontSize,
+                            fontWeight = fontWeight,
+                            textAlign = textAlign ?: TextAlign.Unspecified,
+                            lineHeight = lineHeight,
+                            fontFamily = fontFamily,
+                            textDecoration = textDecoration,
+                            fontStyle = fontStyle,
+                            letterSpacing = letterSpacing,
+                            style = style,
+                        )
+
+                        Box(
+                            modifier =
+                                Modifier
+                                    .graphicsLayer {
+                                        translationY = -(readingFontSize.toPx() * 1.6f)
+                                    },
+                        ) {
+                            if (showReadings) {
+                                Text(
+                                    modifier = Modifier.wrapContentWidth(unbounded = true),
+                                    color = color,
+                                    text = reading,
+                                    fontSize = readingFontSize,
+                                    fontWeight = fontWeight,
+                                    textAlign = textAlign ?: TextAlign.Unspecified,
+                                    lineHeight = lineHeight,
+                                    fontFamily = fontFamily,
+                                    textDecoration = textDecoration,
+                                    fontStyle = fontStyle,
+                                    letterSpacing = letterSpacing,
+                                    style = style,
+                                )
+                            }
+                        }
+                    }
+                },
+            )
+        }
+    } to inlineContent
+}
