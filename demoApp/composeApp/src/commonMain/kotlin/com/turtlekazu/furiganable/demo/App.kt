95ae66fd227a36058f0b53d00d10c227e7be7244
Kazuhiro ISHIBASHI <turtlekazu@icloud.com>
2025-05-18 18:16:21 +0900

    refactor text and add english version preview

--- demoApp/composeApp/src/commonMain/kotlin/com/turtlekazu/furiganable/demo/App.kt
+++ demoApp/composeApp/src/commonMain/kotlin/com/turtlekazu/furiganable/demo/App.kt
@@ -1,12 +1,17 @@
 package com.turtlekazu.furiganable.demo
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.turtlekazu.lib.furiganable.TextWithReadingM2
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -14,40 +19,81 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        Column(
-            Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
         ) {
-            TextWithReadingM2(
-                "これは[試験用[しけんよう]]の[文字列[もじれつ]]です。",
-                style = MaterialTheme.typography.h3,
-            )
+            val text =
+                "これは[試験用[しけんよう]]の[文字列[もじれつ]]です。[複数行[ふくすうぎょう]]にも[対応[たいおう]]していることを[検証[けんしょう]]するために[長[なが]]くしています。"
+            val text_en =
+                "This is a test string. It is long to verify that it supports multiple lines."
+            Column(
+                Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+            ) {
+                TextWithReadingM2(
+                    text,
+                    style = MaterialTheme.typography.h3,
+                )
 
-            TextWithReadingM2(
-                "これは[試験用[しけんよう]]の[文字列[もじれつ]]です。",
-                style = MaterialTheme.typography.h4,
-            )
+                TextWithReadingM2(
+                    text_en,
+                    style = MaterialTheme.typography.h3,
+                )
 
-            TextWithReadingM2(
-                "これは[試験用[しけんよう]]の[文字列[もじれつ]]です。",
-                style = MaterialTheme.typography.h5,
-            )
+                TextWithReadingM2(
+                    text,
+                    style = MaterialTheme.typography.h4,
+                )
 
-            TextWithReadingM2(
-                "これは[試験用[しけんよう]]の[文字列[もじれつ]]です。",
-                style = MaterialTheme.typography.h6,
-            )
+                TextWithReadingM2(
+                    text_en,
+                    style = MaterialTheme.typography.h4,
+                )
 
-            TextWithReadingM2(
-                "これは[試験用[しけんよう]]の[文字列[もじれつ]]です。",
-                style = MaterialTheme.typography.body1,
-            )
+                TextWithReadingM2(
+                    text,
+                    style = MaterialTheme.typography.h5,
+                )
 
-            TextWithReadingM2(
-                "これは[試験用[しけんよう]]の[文字列[もじれつ]]です。",
-                style = MaterialTheme.typography.subtitle1,
-            )
+                TextWithReadingM2(
+                    text_en,
+                    style = MaterialTheme.typography.h5,
+                )
+
+                TextWithReadingM2(
+                    text,
+                    style = MaterialTheme.typography.h6,
+                )
+
+                TextWithReadingM2(
+                    text_en,
+                    style = MaterialTheme.typography.h6,
+                )
+
+                TextWithReadingM2(
+                    text,
+                    style = MaterialTheme.typography.body1,
+                )
+
+                TextWithReadingM2(
+                    text_en,
+                    style = MaterialTheme.typography.body1,
+                )
+
+                TextWithReadingM2(
+                    text,
+                    style = MaterialTheme.typography.subtitle1,
+                )
+
+                TextWithReadingM2(
+                    text_en,
+                    style = MaterialTheme.typography.subtitle1,
+                )
+            }
         }
     }
 }
