56fd8e298432a9082ca330ec92be8d75d877b3d0
Kazuhiro ISHIBASHI <turtlekazu@icloud.com>
2025-05-24 16:44:20 +0900

    add furigana position branching according to os

---
+++ furiganable/src/commonMain/kotlin/com/turtlekazu/lib/furiganable/FuriganaPosition.kt
@@ -0,0 +1,3 @@
+package com.turtlekazu.lib.furiganable
+
+expect fun getFuriganaPosition(): Float
---
+++ furiganable/src/iosMain/kotlin/com/turtlekazu/lib/furiganable/FuriganaPosition.ios.kt
@@ -0,0 +1,5 @@
+package com.turtlekazu.lib.furiganable
+
+actual fun getFuriganaPosition(): Float {
+    return 1.8f
+}
