c68495915dec7f56f7cdeff1268b1a73344dc392
Kazuhiro ISHIBASHI <turtlekazu@icloud.com>
2025-05-25 19:28:18 +0900

    remove unused platform class

--- furiganable/compose-core/src/androidMain/kotlin/com/turtlekazu/furiganable/compose/core/Platform.android.kt
+++
@@ -1,3 +0,0 @@
-package com.turtlekazu.furiganable.compose.core
-
-actual fun platform() = "Android"
--- furiganable/compose-core/src/commonMain/kotlin/com/turtlekazu/furiganable/compose/core/Platform.kt
+++
@@ -1,3 +0,0 @@
-package com.turtlekazu.furiganable.compose.core
-
-expect fun platform(): String
