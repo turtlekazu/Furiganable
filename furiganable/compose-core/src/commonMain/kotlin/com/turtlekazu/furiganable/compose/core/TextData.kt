f0288b29b58dd444bf234eec950b3054ee26d8d2
Kazuhiro ISHIBASHI <turtlekazu@icloud.com>
2025-06-08 21:41:56 +0900

    update Kdoc

--- furiganable/compose-core/src/commonMain/kotlin/com/turtlekazu/furiganable/compose/core/TextData.kt
+++ furiganable/compose-core/src/commonMain/kotlin/com/turtlekazu/furiganable/compose/core/TextData.kt
@@ -1,10 +1,10 @@
 package com.turtlekazu.furiganable.compose.core
 
 /**
- * A data class representing text with optional furigana (phonetic reading).
+ * A data class representing text with optional furigana (phonetic readings).
  *
  * This is used to pass text to components that support displaying both the original text
- * (such as kanji) and its phonetic reading (furigana).
+ * (such as kanji) and its furigana (phonetic readings).
  *
  * @property text The main text to display (e.g., in kanji).
  * @property reading The optional phonetic reading of the text. If `null`, no furigana will be shown.
