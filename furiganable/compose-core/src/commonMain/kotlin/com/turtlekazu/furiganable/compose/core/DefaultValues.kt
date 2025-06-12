cbb2473e0186b117b7282a6acb86310acfa27e77
Kazuhiro ISHIBASHI <turtlekazu@icloud.com>
2025-06-12 21:50:25 +0900

    add file for default values

---
+++ furiganable/compose-core/src/commonMain/kotlin/com/turtlekazu/furiganable/compose/core/DefaultValues.kt
@@ -0,0 +1,10 @@
+package com.turtlekazu.furiganable.compose.core
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * @see androidx.compose.ui.text.SpanStyle
+ */
+const val DEFAULT_FONT_SIZE = 14f
+const val DEFAULT_LETTER_SPACING = 0f
+val DEFAULT_COLOR = Color.Black
