a91174bf6c3747953544931664ddd6730b6edcdd
Kazuhiro ISHIBASHI <turtlekazu@icloud.com>
2025-06-12 22:35:27 +0900

    remove unused comment

--- furiganable/compose-core/src/commonMain/kotlin/com/turtlekazu/furiganable/compose/core/StringExt.kt
+++ furiganable/compose-core/src/commonMain/kotlin/com/turtlekazu/furiganable/compose/core/StringExt.kt
@@ -10,18 +10,15 @@ internal fun String.toTextData(): List<TextData> {
     var lastIndex = 0
 
     regex.findAll(this).forEach { match ->
-        // 通常のテキスト部分を追加
         if (match.range.first > lastIndex) {
             result.add(TextData(this.substring(lastIndex, match.range.first)))
         }
 
-        // ルビ付きテキストを追加
         result.add(TextData(match.groupValues[1], match.groupValues[2]))
 
         lastIndex = match.range.last + 1
     }
 
-    // 残りの通常テキストを追加
     if (lastIndex < this.length) {
         result.add(TextData(this.substring(lastIndex)))
     }
