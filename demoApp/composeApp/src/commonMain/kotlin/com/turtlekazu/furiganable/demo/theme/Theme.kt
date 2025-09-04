84e7c8bb9ab2adde1bfab68694f1f7547d29b46e
Kazuhiro ISHIBASHI <turtlekazu@icloud.com>
2025-09-04 19:50:20 +0900

    add Theme file

---
+++ demoApp/composeApp/src/commonMain/kotlin/com/turtlekazu/furiganable/demo/theme/Theme.kt
@@ -0,0 +1,50 @@
+package com.turtlekazu.furiganable.demo.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightColors = androidx.compose.material3.lightColorScheme(
+    onBackground = Color(0xFF6D0000)
+)
+
+private val DarkColors = androidx.compose.material3.darkColorScheme(
+    onBackground = Color(0xFFFFDADA)
+)
+
+private val LightColorPalette = lightColors(
+    onBackground = Color(0xFF20008A)
+)
+
+private val DarkColorPalette = darkColors(
+    onBackground = Color(0xFFD6C9FF)
+)
+
+@Composable
+fun AppThemeM3(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkColors else LightColors
+
+    androidx.compose.material3.MaterialTheme(
+        colorScheme = colors,
+        content = content
+    )
+}
+
+@Composable
+fun AppThemeM2(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkColorPalette else LightColorPalette
+
+    MaterialTheme(
+        colors = colors,
+        content = content
+    )
+}
